63fcdbe2043486ce91310cce0aa0940371a34470
anthro-main <183952445+anthro-main@users.noreply.github.com>
2025-03-17 08:25:31 -0700

    Grant global TRUSTED_USER permissions instead of board local

--- migrations/1.7.6.js
+++ migrations/1.7.6.js
@@ -2,7 +2,9 @@
 
 module.exports = async(db, redis) => {
 	console.log('Clearning trusted users from boards.');
+	await db.collection('accounts').updateMany({}, { $unset: { trustedBoards: '' } });
 	console.log('Clearning trusted boards from users.');
+	await db.collection('boards').updateMany({}, { $unset: { trusted: ''} });
 	
 	console.log('Clearing globalsettings cache');
 	await redis.deletePattern('globalsettings');
