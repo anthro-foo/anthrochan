125bc59ec2b20efb8a08d87f1477e09857f00a5e
anthro-main <183952445+anthro-main@users.noreply.github.com>
2025-03-17 08:08:44 -0700

    schema check user not staff or already trusted

--- models/forms/addtrusted.js
+++ models/forms/addtrusted.js
@@ -5,18 +5,17 @@ const { Boards, Accounts } = require(__dirname+'/../../db/')
 	, roleManager = require(__dirname+'/../../lib/permission/rolemanager.js');
 
 module.exports = async (req, res) => {
+	const { __, __n } = res.locals;
 
-	const { __ } = res.locals;
-
-	await Promise.all([
-		Accounts.addTrustedBoard([req.body.username], res.locals.board._id),
-		Boards.addTrusted(res.locals.board._id, req.body.username, roleManager.roles.BOARD_STAFF_DEFAULTS)
-	]);
+	/* await Promise.all([
+		// Accounts.addTrustedBoard([req.body.username], res.locals.board._id),
+		// Boards.addTrusted(res.locals.board._id, req.body.username, roleManager.roles.BOARD_STAFF_DEFAULTS)
+	]); */
 
 	return dynamicResponse(req, res, 200, 'message', {
 		'title': __('Success'),
-		'message': __('Added user to trusted'),
-		'redirect': `/${req.params.board}/manage/trusted.html`,
+		'message': __('Trusted user'),
+		'redirect': '/globalmanage/trusted.html',
 	});
 
 };
