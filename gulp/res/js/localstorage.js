52e691c1efe96856383c99c54d0d99208d330d29
blackchandev <183952445+anthro-main@users.noreply.github.com>
2025-04-03 05:04:39 -0700

    update regex for isHome to only match /index.html at start of string

--- gulp/res/js/localstorage.js
+++ gulp/res/js/localstorage.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-unused-vars */
 /* globals setDefaultLocalStorage settings */
-const isHome = /index.html/.test(window.location.pathname);
+const isHome = /^\/index.html/.test(window.location.pathname);
 const isCatalog = /^\/(\w+\/(manage\/)?)?catalog.html/.test(window.location.pathname);
 const isThread = /\/\w+\/thread\/\d+(\+50)?.html/.test(window.location.pathname);
 const isModView = /\/\w+\/manage\/(thread\/)?(index|\d+)(\+50)?.html/.test(window.location.pathname);
